ae201ef2fec2aade97e6e9e940cf9a4b9f0ff5ff
Sergey Fedotov <sergey.fedotov@convosight.com>
2025-04-07 12:25:19 +0300

    basic lodash connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+const lodash = require('lodash')
+// maven - java
+// pip/pip3 - python
+// NuGet - C#
 console.log('hello world')
+
+
+const arrayNumbers = [1,1,2,2,3,3,4,4,5,5,6];
+
+const uniqNumbers = lodash.uniq(arrayNumbers);
+console.log(uniqNumbers);
+
+const setNumbers = Array.from(new Set(arrayNumbers));
+console.log(setNumbers);
